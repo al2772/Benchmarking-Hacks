f3baaf0499c6eee96d9c532ac0cba95240c63e95
Adam Lewandowski <98980249+al2772@users.noreply.github.com>
2023-08-21 18:04:26 -0500

    Update LockBox.java

--- LockBox.java
+++ LockBox.java
@@ -1,29 +1,3 @@
-//////////////// FILE HEADER (INCLUDE IN EVERY FILE) //////////////////////////
-//
-// Title:    LockBox object that is to be hacked by the other classes
-// Course:   CS 300 Fall 2021
-//
-// Author:   Adam Lewandowski
-// Email:    aclewandowsk@wisc.edu
-// Lecturer: Hobbes LeGault
-//
-//////////////////// PAIR PROGRAMMERS COMPLETE THIS SECTION ///////////////////
-//
-// Partner Name:    (name of your pair programming partner)
-// Partner Email:   (email address of your programming partner)
-// Partner Lecturer's Name: (name of your partner's lecturer)
-// 
-// VERIFY THE FOLLOWING BY PLACING AN X NEXT TO EACH TRUE STATEMENT:
-//   ___ Write-up states that pair programming is allowed for this assignment.
-//   ___ We have both read and understand the course Pair Programming Policy.
-//   ___ We have registered our team prior to the team registration deadline.
-//
-///////////////////////// ALWAYS CREDIT OUTSIDE HELP //////////////////////////
-//
-// Persons:         (identify each by name and describe how they helped)
-// Online Sources:  (identify each by URL and describe how it helped)
-//
-///////////////////////////////////////////////////////////////////////////////
 import java.util.Random;
 /**
  * 
